c276acc292b43381632298cdaab428e74dbd1030
BenjaminHogedal <eaa25bvh@students.eaaa.dk>
2025-09-04 09:09:20 +0200

    Første gang

---
+++ Bjarnesburger/css.css
@@ -0,0 +1,110 @@
+/**
+ * file: css/css.css
+ * purpose: styling
+ **/
+
+/* Remove whitespace on top of certain browsers */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body
+  /*Skrifttype/*
+font-family: Verdana, Geneva, Tahoma, sans-serif;
+font-size: 1.5rem;
+}
+
+/* Define variables in the :root */
+ 
+
+/* Use your variables on elements along these lines */
+/*
+:root {
+  --contrast: lime;
+  --backgr: #000;
+}
+
+--- You can use the variables like this ---
+
+ main {
+   color: var(--contrast);
+   background-color: var(--backgr);
+ }
+*/
+
+h1{
+color: black (16, 16, 16);
+font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+
+h2 {
+  color: black (16, 15, 15);
+}
+
+p {
+  color: black;
+}
+
+/*Billeder i 100% */
+img {
+width: 100%;
+}
+
+.flex {
+  display: flex;
+}
+
+#topNav img{
+  width: 112px;
+  height: auto;
+}
+
+#topNav a {
+  color: black;
+} 
+#topNav a:hover {
+  color: black;
+}
+
+/* hero sektionen styles */
+header {
+  height: 100vh;
+  background-color:white;
+
+/* Herobillede i baugrunden */
+background-image: url('../images/hero.png');
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-size: cover;
+background-position: center center;
+
+/* Læg tekst i midten vertikalt og horisontalt */
+display: flex;
+justify-content: center;
+align-items: center;
+}
+
+
+/* RESPONSIVT DESIGN */
+
+/* Mobile enheder */
+@media (max-width: 600px){
+  body {
+  background-color: red; 
+}
+  #stemning {
+    display: block;
+  }
+}
+
+/*tablets, ipads, &c.*/
+@media (min-width :601px) and (max-width: 1024px){
+  body{
+  background-color: red
+  }
+  #stemning {
+    display: block;
+  }
+}
